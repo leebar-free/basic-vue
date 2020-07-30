be72562a18eed056b11c84f69c1bece797d4638d
ms lee <leebar@naver.com>
2020-07-30 14:02:41 +0900

    sss

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about2',
+    name: 'About2',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About2.vue')
   }
 ]
 
